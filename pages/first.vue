df40fbb9645b1bfcad68be4b7ab4fff76782d734
zedparman <58415963+zedparman@users.noreply.github.com>
2020-12-28 14:30:19 +0330

    first commit

---
+++ pages/first.vue
@@ -0,0 +1,39 @@
+<template>
+    <div  class="container">
+        <h1>Hello</h1>
+        <ul>
+         <li v-for="s in students" :key="s.family">
+         {{ s.nam }}
+         </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            students: []
+        }
+    },
+    methods: {
+        async studentsList(){
+            try{
+                let response = await axios.get('http://test3.egpayesh.ir/students')
+                this.students = response.data
+                console.log(response)
+            } catch(error) {
+                console.log(error)
+            }
+        }   
+    },
+    mounted(){
+      this.studentsList()
+    }
+}
+</script>
+
+<style>
+
+</style>
